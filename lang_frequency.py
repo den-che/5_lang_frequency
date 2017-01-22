1f825501b47faa8aa2de62cce956bae636c47085
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-01-22 16:10:03 +0300

    Change variable's names in code

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
@@ -9,25 +10,23 @@ def load_data(filepath):
         return data	
 
 
-
 def get_most_frequent_words(text):
-    dict_frequency = dict()
+    word_frequency = dict()
     cleared_text = re.findall(r'[^\d|\W]+', text)
     for word in cleared_text:
-        if word in dict_frequency:
-            dict_frequency[word]+=1
+        if word in word_frequency:
+            word_frequency[word]+=1
         else:
-            dict_frequency[word] = 1
-    return dict_frequency
+            word_frequency[word] = 1
+    return word_frequency
 
 
 if __name__ == '__main__':
     filepath = input("Введите путь к файлу: ")
     text = load_data(filepath)
-    
     if text is None:
         print("Файла или папки с таким названием не существует.")
     else:    
-        freq_words = get_most_frequent_words(text)
-        for key, value in sorted(freq_words.items(), key=lambda val:val[1], reverse=True)[:10]:
-            print ("{} {}".format(key, value))    
+        frequency_words = get_most_frequent_words(text)
+        for word, frequency in sorted(frequency_words.items(), key=lambda val:val[1], reverse=True)[:10]:
+            print ("{} {}".format(word, frequency))    
